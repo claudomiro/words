56f45928004c5b0cacd803e0619cd3aa60cd1403
claudomiro <claudomiro@gmail.com>
2020-04-01 12:39:01 -0300

    Passo 06 Implementação

--- src/main/java/org/claudomiro/tdd/words/CurrencyConverter.java
+++ src/main/java/org/claudomiro/tdd/words/CurrencyConverter.java
@@ -11,7 +11,11 @@ public class CurrencyConverter
     public String conv(double currencyValue)
     {
         int integerValue = (int) currencyValue;
+        int centsValue = (int) ((currencyValue - integerValue) *  100.00);
 
-        return integerConverter.conv(integerValue) + " reais";
+        if(centsValue == 0) {
+            return integerConverter.conv(integerValue) + " reais";
+        }
+        return integerConverter.conv(centsValue) + " centavos";
     }
 }
