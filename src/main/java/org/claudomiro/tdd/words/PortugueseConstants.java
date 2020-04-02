81af56cce53d8ab473396726c2eacfa28795fbe0
claudomiro <claudomiro@gmail.com>
2020-04-02 03:17:09 -0300

    Passo 08 Especificação

--- src/main/java/org/claudomiro/tdd/words/PortugueseConstants.java
+++ src/main/java/org/claudomiro/tdd/words/PortugueseConstants.java
@@ -1,8 +1,12 @@
 package org.claudomiro.tdd.words;
 
 interface PortugueseConstants {
-    String CURRENCY = "reais";
-    String CENTS = "centavos";
+    String CURRENCY_SINGULAR = "real";
+    String CURRENCY_PLURAL = "reais";
+
+    String CENT_SINGULAR = "centavo";
+    String CENT_PLURAL = "centavos";
+
     String CONECTOR = "e";
     String SPACE = " ";
     double ONE_HUNDRED = 100.00;
